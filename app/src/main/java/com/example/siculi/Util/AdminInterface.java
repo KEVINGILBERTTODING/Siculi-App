0de2eca173f7b99ff1585220db9ea411845e8c58
KEVINGILBERTTODING <122202002832@mhs.dinus.ac.id>
2023-05-29 01:41:05 +0700

    added setup app environtment

---
+++ app/src/main/java/com/example/siculi/Util/AdminInterface.java
@@ -0,0 +1,4 @@
+package com.example.siculi.Util;
+
+public interface AdminInterface {
+}
---
+++ app/src/main/java/com/example/siculi/Util/DataApi.java
@@ -0,0 +1,19 @@
+package com.example.siculi.Util;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class DataApi {
+    public static final String IP_ADDRESS = "192.168.100.6"; // isi ip address
+    public static final String BASE_URL = "http://" + IP_ADDRESS + "/siculi/";
+    public Retrofit retrofit = null;
+    public Retrofit getClient() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
