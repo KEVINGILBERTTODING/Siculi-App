959b265d93faf6e5100d3c1a4467fcc70ed51e84
KEVINGILBERTTODING <122202002832@mhs.dinus.ac.id>
2023-06-04 11:26:21 +0700

    redesign splash screen

--- app/src/main/java/com/example/siculi/SplashScreen.java
+++ app/src/main/java/com/example/siculi/SplashScreen.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -12,6 +13,10 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION|
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
